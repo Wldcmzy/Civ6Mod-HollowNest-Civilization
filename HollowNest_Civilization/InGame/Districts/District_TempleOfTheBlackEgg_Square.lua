63cb49a3be852875f722b028368f54546ce59dd0
StarsWhisper <wldcmzy@163.com>
2024-03-15 15:33:48 +0800

    first commit

---
+++ HollowNest_Civilization/InGame/Districts/District_TempleOfTheBlackEgg_Square.lua
@@ -0,0 +1,30 @@
+-- District_TempleOfTheBlackEgg_Square
+-- Author: wldcmzy
+-- DateCreated: 7/21/2023 10:54:02 PM
+--------------------------------------------------------------
+
+function HollowNest_ShowText_Memory_of_The_Abyss(playerID, districtID, iX, iY)
+    if (districtID == GameInfo.Districts["DISTRICT_TEMPLE_OF_THE_BLACK_EGG_SQUARE"].Index) then
+		print('Temple of the Black Egg Square constructed...')
+		local pPlayer = Players[playerID]
+		pPlayer:GetUnits():Create(GameInfo.Units['UNIT_HOLLOW_KNIGHT'].Index, iX, iY)
+		print('One Free Hollow Knight Created...')
+		local TextVarPrefix = 'LOC_TEXT_MEMORY_OF_THE_AYBSS_'
+		local LeaderRadianceSuffix = "_RADIANCE"
+		local txt = ""
+		local playerCfg = PlayerConfigurations[playerID]
+		local leaderType = playerCfg:GetLeaderTypeName()
+		print('Start to show Text of Memory_of_The_Abyss')
+		for i=0,8 do
+			txt = TextVarPrefix .. i
+			if (i == 6) and (leaderType == "LEADER_RADIANCE") then
+				print('Show Radiance Trait Text')
+				txt = TextVarPrefix .. i .. LeaderRadianceSuffix
+			end
+			txt = Locale.Lookup(txt)
+			Game.AddWorldViewText(0, txt, iX, iY)
+		end
+    end
+end
+
+GameEvents.OnDistrictConstructed.Add(HollowNest_ShowText_Memory_of_The_Abyss);
